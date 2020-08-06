a76746b25d855b4c7043b4ae1dffb1749a3638ad
MGrey <grey@pku.edu.cn>
2020-08-06 20:35:09 +0800

    fix scihub

--- downloader.py
+++ downloader.py
@@ -5,13 +5,56 @@ import json
 import requests
 from lxml import etree
 import random
-import scihub
+
+def random_headers():
+    version=random.randint(70, 83)
+    HEADERS={
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh-TW;q=0.7,zh;q=0.6',
+        'cache-control': 'max-age=0',
+        'cookie': '__ddg1=9ecPxT9UnphsDYkzdiPP; session=ace4fd2a85e617e3b21475d6a75e1626; refresh=1596419755.705; __ddg2=huX54o8URarDaO3R',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': f'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{version}.0.3987.163 Safari/537.36',
+    }
+    return HEADERS
 
 
-firefox_version=random.randint(50, 80)
-HEADERS={
-    'User-Agent':f'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:{firefox_version}.0) Gecko/20100101 Firefox/{firefox_version}.0',
-}
+class SciHub():
+    def __init__(self):
+        self.sess=requests.session()
+        self.pat=re.compile(r"location.href=\'(.+?)\'")
+    
+    def _get_pdf_url(self, paper_url, path):
+        pg=''
+        try:
+            pg=self.sess.get(f'https://sci-hub.tw/{paper_url}', headers=random_headers()).text
+        except Exception as e:
+            print('[网络问题]无法访问sci-hub.tw', e, paper_url, path)
+        
+        if pg:
+            match=self.pat.search(pg)
+            if isinstance(match, re.Match):
+                pdf_url=match.group(1)
+                if not pdf_url.startswith('https'):
+                    pdf_url=f'https:{pdf_url}'
+                return pdf_url
+            else:
+                print('[网络问题]存在验证码，下次重启再试', paper_url, path)
+    
+    def download(self, paper_url, path):
+        pdf_url=self._get_pdf_url(paper_url, path)
+        try:
+            pdf=self.sess.get(pdf_url, headers=random_headers()).content
+            with open(path, 'wb') as file:
+                file.write(pdf)
+        except FileNotFoundError as e:
+            print(f'本地无法保存文件{path}', e, paper_url, path)
+        except Exception as e:
+            print('[网络问题]无法下载pdf', e, paper_url, path)
 
 def get_volume_issue_urls(start_year, end_year):
     '''get volume-issues of every year'''
@@ -19,7 +62,7 @@ def get_volume_issue_urls(start_year, end_year):
     for year in range(start_year, end_year+1):
         os.makedirs(str(year), exist_ok=True)
         url=f'https://www.sciencedirect.com/journal/00223115/year/{year}/issues'
-        j_urls=requests.get(url, headers=HEADERS).json()
+        j_urls=requests.get(url, headers=random_headers()).json()
         all_vi_urls[year]=[f"https://www.sciencedirect.com/journal/journal-of-nuclear-materials{url['uriLookup']}" for url in j_urls['data']]
     
     print('Success to get all volume-issue urls')
@@ -29,7 +72,7 @@ def get_all_paper_urls(all_vi_urls):
     all_paper_urls=[]
     for year in all_vi_urls:
         for vi_url in all_vi_urls[year]:
-            vi_page=requests.get(vi_url, headers=HEADERS).text
+            vi_page=requests.get(vi_url, headers=random_headers()).text
             tree=etree.HTML(vi_page)
             raw_urls = tree.xpath("//a[contains(@class, 'article-content-title')]/@href")
             raw_titles=tree.xpath("//a[contains(@class, 'article-content-title')]/span")
@@ -62,7 +105,7 @@ if __name__ == "__main__":
     start_year=int(sys.argv[1])
     end_year=int(sys.argv[2])
 
-    hub=scihub.SciHub()
+    hub=SciHub()
     all_vi_urls=get_volume_issue_urls(start_year, end_year)
     all_paper_urls=get_all_paper_urls(all_vi_urls)
 
@@ -70,7 +113,7 @@ if __name__ == "__main__":
         filename=f'{year}/volume{volume}-issue{issue}/{url[50:]}.pdf'
         if not os.path.exists(filename):
             hub.download(url, path=filename)
-        print(f'finished--->{url}, {title}')
+        print(f'finished--->{url}, {title[:60]}')
     print(f'finishe all paper from {start_year} to {end_year}')
 
 
